3003ed74865a2dabb849ca5a98a54028fcbf2ed1
shuaibcs <alshuaibi7@gmail.com>
2019-09-09 15:37:24 +0200

    Welcome to Codaisseur

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hala Hala Shuaib
+          'Welcome to Codaisseur'
         </p>
         <a
           className="App-link"
